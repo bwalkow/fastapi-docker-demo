807b7c4f5374af8d5f59ba229ff9734527a4b687
bwalkow <bartosz.walkowiak@pwr.edu.pl>
2025-07-06 16:12:00 +0200

    Update

--- app/main.py
+++ app/main.py
@@ -9,8 +9,12 @@ DB_PASS = os.environ.get("POSTGRES_PASSWORD")
 DB_HOST = "db"
 DB_NAME = "postgres"
 
-@app.get("/")
-async def root():
+@app.get("/status")
+async def status():
+    return {"status": "ok"}
+
+@app.get("/db")
+async def db():
     conn = await asyncpg.connect(user=DB_USER, password=DB_PASS,
                                  host=DB_HOST, database=DB_NAME)
     now = await conn.fetchval("SELECT NOW()")
